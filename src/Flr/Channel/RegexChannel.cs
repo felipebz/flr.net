254c279cdd12b253632e983e4a0581306e9162a0
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2023-11-23 23:19:56 -0300

    Add RegexChannel

---
+++ src/Flr/Channel/RegexChannel.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Flr.Channel;
+
+public abstract class RegexChannel<T>(Regex myRegex) : IChannel<T>
+{
+    private readonly StringBuilder _builder = new();
+
+    protected RegexChannel(string regex) : this(new Regex($"^{regex}", RegexOptions.Compiled))
+    {
+    }
+
+    public bool Consume(CodeReader code, T output)
+    {
+        if (code.PopTo(myRegex, _builder) > char.MinValue)
+        {
+            Consume(_builder, output);
+            _builder.Clear();
+            return true;
+        }
+
+        return false;
+    }
+
+    protected abstract void Consume(StringBuilder builder, T output);
+}
